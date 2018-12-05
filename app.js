795f8b61c8fba5d8e322ae4773ffd634bf9b248b
= <mflohost@gmail.com>
2018-12-05 17:34:26 +0200

    right now i can view a single post

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'node_modules/popper.js/dist')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/new-post',newPostRouter);
-app.use('/posts/:post_id',postsRouter);
+app.use('/posts',postsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Post = require('../databases/post-collection');
+var url = require('url');
+var queryString = require('querystring');
 
 /* GET users listing. */
-router.get('/', async (req, res, next)=> {
+router.get('/:post_id', async (req, res, next)=> {
   
-  const post = await Post.findById(req.params.user_id);
-  res.render('post',{title:post.title,post});
+  const post = await Post.findById(req.params.post_id);
+  res.render('post',{title:'blog-post',post});
   
 });
 
