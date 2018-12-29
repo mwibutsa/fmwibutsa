1290b0453b4d7faa981f0e2b2356cfa810168416
mwibutsa <mflohost@gmail.com>
2018-12-29 05:41:27 +0200

    Making it better

--- public/javascripts/custom-script.js
+++ public/javascripts/custom-script.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-	$('#footer').css('margin-top',$(document).height()-$("#wrapper").height()+$('#footer').height() -227);
+	//$('#footer').css('margin-top',$(document).height()-$("#wrapper").height()+$('#footer').height() -227);
 });
-
-function customReady(){
-	alert('test my custom ready');
-}
