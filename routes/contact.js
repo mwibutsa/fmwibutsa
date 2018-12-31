20a90197419ce30f683f80e39bb272356d561365
mwibutsa <mflohost@gmail.com>
2018-12-31 17:23:38 +0200

    removing an errot

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ContactCollection = require('../databases/contact-collection');
 var nodeMailer = require('nodemailer');
-var transport = nodeMailer.createTransport('SMTP',{
+var transport = nodeMailer.createTransport({
 	service:'gmail',
 	auth:{
 		user:'mflohost@gmail.com',
